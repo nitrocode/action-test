413aa0cb9266c3c87b92514cffc232f34d654d35
Dylan Bannon <dylanbannon5@gmail.com>
2022-01-13 17:09:35 -0800

    patching tf code to gets bats tests to pass

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,22 +1,22 @@
-output "role_name" {
-  value       = module.role.name
-  description = "The name of the created role"
-}
+#output "role_name" {
+#  value       = module.role.name
+#  description = "The name of the created role"
+#}
 
-output "role_id" {
-  value       = module.role.id
-  description = "The stable and unique string identifying the role"
-}
+#output "role_id" {
+#  value       = module.role.id
+#  description = "The stable and unique string identifying the role"
+#}
 
-output "role_arn" {
-  value       = module.role.arn
-  description = "The Amazon Resource Name (ARN) specifying the role"
-}
+#output "role_arn" {
+#  value       = module.role.arn
+#  description = "The Amazon Resource Name (ARN) specifying the role"
+#}
 
-output "role_policy" {
-  value       = module.role.policy
-  description = "The Amazon Resource Name (ARN) specifying the role"
-}
+#output "role_policy" {
+#  value       = module.role.policy
+#  description = "The Amazon Resource Name (ARN) specifying the role"
+#}
 
 output "key_arn" {
   value       = module.kms_key.key_arn
