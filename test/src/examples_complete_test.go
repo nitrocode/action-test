5263b667b293fd705b6979e6c105513f71d3f5ba
Dylan Bannon <dylanbannon5@gmail.com>
2022-03-01 01:29:59 -0800

    updating test files based on terraform-aws-elasticache-memcached repo

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -1,10 +1,7 @@
 package test
 
 import (
-	"math/rand"
-	"strconv"
 	"testing"
-	"time"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -14,20 +11,12 @@ import (
 func TestExamplesComplete(t *testing.T) {
 	t.Parallel()
 
-	rand.Seed(time.Now().UnixNano())
-
-	randId := strconv.Itoa(rand.Intn(100000))
-	attributes := []string{randId}
-
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: "../../examples/complete",
 		Upgrade:      true,
 		// Variables to pass to our Terraform code using -var-file options
 		VarFiles: []string{"fixtures.us-east-2.tfvars"},
-		Vars: map[string]interface{}{
-			"attributes": attributes,
-		},
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
@@ -37,19 +26,22 @@ func TestExamplesComplete(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of an output variable
-	keyArn := terraform.Output(t, terraformOptions, "key_arn")
+	vpcCidr := terraform.Output(t, terraformOptions, "vpc_cidr")
 	// Verify we're getting back the outputs we expect
-	assert.Contains(t, keyArn, "arn:aws:kms:us-east-2")
+	assert.Equal(t, "172.16.0.0/16", vpcCidr)
 
 	// Run `terraform output` to get the value of an output variable
-	aliasName := terraform.Output(t, terraformOptions, "key_alias_name")
-	expectedAliasName := "alias/eg-test-iam-role-test-" + randId
+	privateSubnetCidrs := terraform.OutputList(t, terraformOptions, "private_subnet_cidrs")
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, expectedAliasName, aliasName)
+	assert.Equal(t, []string{"172.16.0.0/19"}, privateSubnetCidrs)
 
 	// Run `terraform output` to get the value of an output variable
-	s3BucketId := terraform.Output(t, terraformOptions, "bucket_id")
-	expectedS3BucketId := "eg-test-iam-role-test-" + randId
+	publicSubnetCidrs := terraform.OutputList(t, terraformOptions, "public_subnet_cidrs")
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, expectedS3BucketId, s3BucketId)
+	assert.Equal(t, []string{"172.16.96.0/19"}, publicSubnetCidrs)
+
+	// Run `terraform output` to get the value of an output variable
+	clusterId := terraform.Output(t, terraformOptions, "cluster_id")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "eg-test-memcached-test", clusterId)
 }
